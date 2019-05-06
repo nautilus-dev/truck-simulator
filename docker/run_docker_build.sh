f78cc126033152e98be0cc8d3e09aef95a46b322
nautilus-dev <nautilus-dev@users.noreply.github.com>
2019-05-06 10:09:42 +0200

    changed locations

--- docker/run_docker_build.sh
+++ docker/run_docker_build.sh
@@ -1,2 +1,2 @@
-sudo docker build -t trivadispf/truck-simulator ./dockerfile
+sudo docker build -t trivadispf/truck-simulator .
 sudo docker tag trivadispf/truck-simulator:latest trivadispf/truck-simulator:beta
