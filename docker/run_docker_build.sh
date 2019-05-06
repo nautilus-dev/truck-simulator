5345bf5c44887e2d31b6d1ab0ca024753f69e97c
nautilus-dev <nautilus-dev@users.noreply.github.com>
2019-05-06 09:58:20 +0200

    adding new tag

--- docker/run_docker_build.sh
+++ docker/run_docker_build.sh
@@ -1,2 +1,2 @@
 sudo docker build -t trivadispf/truck-simulator ./dockerfile
-sudo docker tag trivadispf/truck-simulator:latest
+sudo docker tag trivadispf/truck-simulator:latest trivadispf/truck-simulator:beta
